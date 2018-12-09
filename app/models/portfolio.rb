72d1f129288f4a17a6f61ee25ba00ba3a12d2fc3
Ruby <ruby@Rubys-iMac.local>
2018-12-09 07:47:17 -0800

    image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,19 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.angular
+    where(subtitle:"Angular")
+  end
+
+  scope :ruby_on_rails_portfolio_items, -> {where(subtitle:"Ruby")}
+
+  after_initialize :set_default
+
+  def set_default
+    self.main_image ||= Placeholder.image_generator(height:'400', width:'600')
+    self.thumb_image ||= Placeholder.image_generator(height:'200', width:'350')
+  end
+
 end
