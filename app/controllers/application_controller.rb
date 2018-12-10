1bf1dac6700ceb52bfeec443e6f7334a19c95db0
Ruby <ruby@Rubys-iMac.local>
2018-12-10 11:03:21 -0800

    deep dive current User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+
+  def current_user
+    super || OpenStruct.new(name:'Guest user', first_name: 'Guest', last_name: 'user')
+  end
+
 end
